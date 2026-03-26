c5e2e55f4ba67837fed770d7703f93bd587d8da0
edduar002 <128913974+edduar002@users.noreply.github.com>
2026-03-26 12:58:50 -0500

    Se concluye parcialmente proyecto/actividad

--- index.js
+++ index.js
@@ -196,5 +196,5 @@ Pregunta: ${pregunta}`,
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor corriendo en puerto ${PORT} 🚀`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
